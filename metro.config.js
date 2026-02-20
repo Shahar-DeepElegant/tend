18edecfa48b1b4ea7977ebac595246e433c76a19
Shahar Shalev <shaharshalev0@gmail.com>
2026-02-20 13:30:58 +0200

    fix: build

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,18 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+const { transformer, resolver } = config;
+
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
+};
+
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+  sourceExts: [...resolver.sourceExts, "svg"],
+};
 
 // Add wasm asset support
 config.resolver.assetExts.push("wasm");
